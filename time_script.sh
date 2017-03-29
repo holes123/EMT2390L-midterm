4fc4bcdffee1c1b0a9b5ec26d990f066e31292f6
holes123 <huang.h0709@gmail.com>
2017-03-29 03:28:32 -0400

    EMT2390L-midterm

--- time_script.sh
+++ time_script.sh
@@ -1,7 +1,7 @@
 # Shows Epoch time in milliseconds
 # CTRL-C TO STOP
 
-#!/bin/js
+#!/bin/bash
 
 
 # Runs command for determining epoch time
@@ -11,9 +11,13 @@ do
         echo $(($(date +%s%N)/1000000))
 
 # Extra credit - add additional lines that output to the terminal the UTC time
-        
+        NOW=$(date +"The time is %M minutes past %I %p U T C")
+	echo $NOW
 
 # Required step: Modify the sleep function to include 1, 5, and 20-second increments while showing console output for each of the different increments
-        sleep 10
-        echo "10"
+        sleep 10s
+	sleep 1s
+	sleep 5s
+	sleep 20s
+        echo "1s, 5s. 20s, 10s"
 done
