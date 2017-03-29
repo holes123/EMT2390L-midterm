c0fc0d74ab3b3dc9503e42ed923115046b12dcf1
Charles Hamilton <HudsonWerks@users.noreply.github.com>
2017-03-29 13:25:44 -0400

    Create time_script.sh

---
+++ time_script.sh
@@ -0,0 +1,20 @@
+# Shows Epoch time in milliseconds
+# CTRL-C TO STOP
+
+#!/bin/bash
+
+
+# Runs command for determining epoch time
+for (( ; ; ))
+do
+        echo -n "Since Epoch [in milliseconds]: "
+        echo $(($(date +%s%N)/1000000))
+
+# Extra credit - add additional lines that output to the terminal the UTC time
+        echo -n "UTC Time: "
+        echo $(date -u)
+
+# Required step: Modify the sleep function to include 1, 5, and 20-second increments while showing console output for each of the different increments
+        sleep 10
+        echo "10"
+done
